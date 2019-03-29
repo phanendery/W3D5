eb7f3f0312ad3f97ea22529a8aecdfb24f968d95
phanendery <phanendery@gmail.com>
2019-03-29 16:44:52 -0700

    fin proj

--- ActiveRecordLite/lib/00_attr_accessor_object.rb
+++ ActiveRecordLite/lib/00_attr_accessor_object.rb
@@ -1,6 +1,7 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     names.each do |name|
+      
       self.instance_variable_get(@name)
       self.instance_variable_set(@name,val)
     end
